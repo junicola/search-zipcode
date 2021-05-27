8d3eede0db46202a1f8ff44064e5189897bf5ab2
Júlia Nicola Gualeve <julia.nicola@hotmail.com>
2021-05-27 00:00:08 -0300

    configurando acesso ao BD

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 
 Aplicação web para consulta de cep na API ViaCEP.
 
-Acesse no navegador o arquivo "index.html" localizado na pasta Front para visualizar a página web e fazer sua consulta. 
+Acesse no navegador o arquivo "index.html" localizado na pasta Front para visualizar a página web e fazer sua consulta. Adicione as configurações do seu banco de dados Firebase em "index.js".
 
 Requisitos </br>
 [x] consultar cep pela API ViaCEP e preencher os campos;</br>
 [x] salvar cache;</br>
-[ ] salvar consultas feitas em um BD externo;</br>
-[ ] consultas já realizadas devem receber os dados do BD;</br>
+[x] salvar consultas feitas em um BD externo;</br>
+[x] consultas já realizadas devem receber os dados do BD;</br>
 
 Extras</br>
 [ ] teste da funcionalidade;</br>
-[ ] boa experiência para o usuário (layout da tela, feedback de ações).</br>
+[x] boa experiência para o usuário (layout da tela, feedback de ações).</br>
--- api/server.js
+++ api/server.js
@@ -1,35 +1,98 @@
-//requisitando dados da API ViaCEP
-function getCEP(){
+// configuração do firebase
+var firebaseConfig = {
+    //adicione aqui informações da sua conta firebase
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  };
+
+
+// inicializar firebase
+firebase.initializeApp(firebaseConfig);
+var cepRef = firebase.database().ref('cep');
+
+
+//requisitando CEP
+function getCEP() {
     var cep = document.getElementById('cep').value;
-    let cacheName = 'cacheCPF'; 
+    let cacheName = 'cacheCPF';
     let url = `https://viacep.com.br/ws/${cep}/json/`;
-
-
-    caches.open(cacheName).then( cache => {
-        cache.add(url).then( () => {
-            fetch(url)
-            .then((response) => {
-                response.json()
-                .then(data => fillData(data))
-            })
-            .catch(error => {
-                console.log('Erro: '+error.message);
-            })
-            console.log("Data cached ")
-        });
-    });
+    const validarCep = /^[0-9]{8}$/;
+    var flag = false;
     
+    if(!validarCep.test(cep)) {
+        document.getElementById('error-message').classList.remove("hide");
+        document.getElementById('error-message').classList.add("show");
+        fillData("");
+    } else {
+        caches.open(cacheName).then(cache => {
+            
+            //verifica se já existe no BD o cep procurado
+            cepRef.on("value", function(snapshot){
+                snapshot.forEach(function(element){
+                    if(cep == element.val().cep){
+                        document.getElementById('error-message').classList.remove("show");
+                        document.getElementById('error-message').classList.add("hide");
+                        flag = true;
+                        fillData(element.val());
+                    } 
+                })
+                if(!flag) {
+                    cache.add(url).then(() => {
+                        fetch(url)
+                        .then((response) => {
+                            response.json()
+                                .then(data => {
+                                    if(data.erro){
+                                        document.getElementById('error-message').classList.remove("hide");
+                                        document.getElementById('error-message').classList.add("show");
+                                        fillData("");
+                                    } else {
+                                        document.getElementById('error-message').classList.remove("show");
+                                        document.getElementById('error-message').classList.add("hide");
+                                        fillData(data);
+                                        saveCep((data.cep).replace("-",""), data.logradouro, data.localidade, data.bairro);
+                                    }
+                                })
+                        }).catch(function(error) {
+                            return error;
+                        })
+                    });
+                }
+            })
+    
+            
+        });
+    }
+
+}
+
+//salvar cep no firebase
+function saveCep(cep, logradouro, localidade, bairro){
+    var newCepRef = cepRef.push();
+    newCepRef.set({
+        cep: cep,
+        logradouro: logradouro,
+        localidade: localidade,
+        bairro: bairro
+    })
 }
 
 //preenchendo os campos do Resultado
-function fillData(data){
-    console.log(data);
+function fillData(data) {
     const municipio = document.getElementById('municipio');
     const logradouro = document.getElementById('logradouro');
     const bairro = document.getElementById('bairro');
-    municipio.value = data.localidade;
-    logradouro.value = data.logradouro;
-    bairro.value = data.bairro;
+    if(data==""){
+        municipio.value = " ";
+        logradouro.value = " ";
+        bairro.value = " ";
+    } else {
+        municipio.value = data.localidade;
+        logradouro.value = data.logradouro;
+        bairro.value = data.bairro;
+    }
 }
-
-//adicionando no banco de dados
